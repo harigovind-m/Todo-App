313f3acf0b7018e5032887d5eb99c12a280d75cd
Hari Govind <harigovind0221@gmail.com>
2024-04-30 16:52:24 +0300

    Updated with nodemon and express

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const app = express()
+const dotenv = require("dotenv").config()
+
+const port = process.env.PORT || 4001
+app.listen(port)
+console.log(`Server Listening to`,port)
+
+//View Engine Configuration
+app.set("view engine", "ejs")
+
+//Setting public files
+app.use(express.static("public"))
+
+app.get("/", (req,res) => {
+    res.render("home")
+})
+
+app.get("/create-task", (req,res) => {
+    res.render("createTask")
+})
